51096aacca1da50f67235d05adefaee651853d16
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-27 21:24:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,8 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-
+  planeteer_calls.collect do |call|
+  call[0].upcase
 end
 
 def long_planeteer_calls# code an argument here
