4e1f8223b70b40246f323ccc2ed0c243e76c50e0
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-27 21:22:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,11 +2,12 @@ def roll_call_dwarves(array_of_dwarves)# code an argument here
   # Your code here
   array_of_dwarves.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
-  end 
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+
 end
 
 def long_planeteer_calls# code an argument here
