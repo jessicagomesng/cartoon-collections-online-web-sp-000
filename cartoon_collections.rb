8ed051ba2ff1dc624c3a46738dc65debb131e3d4
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-27 21:28:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,13 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
+  collection_of_calls = []
+  planeteer_calls.collect do |call|
+    if call.length > 4
+      true
+
 end
 
 def find_the_cheese# code an argument here
