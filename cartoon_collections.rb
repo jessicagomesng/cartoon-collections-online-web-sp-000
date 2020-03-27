44e269933f9b17373e70e496090877d9e99b717b
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-27 21:36:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -36,6 +36,6 @@ def find_the_cheese(array_of_snacks)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
 
   array_of_snacks.detect do |snack|
-    snack == "cheddar" || "gouda" || "camembert"
-  end 
+    snack == "cheddar" || snack == "gouda" || "camembert"
+  end
 end
